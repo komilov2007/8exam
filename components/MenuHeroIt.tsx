aa928837e92d8381874d372abe7e06dce508f2d4
Rahmatulloh <komilov2007.2007@gmailcom>
2026-03-13 03:39:44 +0500

    added

--- components/MenuHeroIt.tsx
+++ components/MenuHeroIt.tsx
@@ -6,7 +6,7 @@ const MenuHeroItem = ({ image, text, name, price }: NewsCardProps) => {
   return (
     <div className="relative w-65.75 h-77.5 mb-20 rounded-[38px] bg-white/30 backdrop-blur-md px-5 pb-5 pt-14 shadow-md">
       <div className="absolute w-65.75 h-49.25 -top-17 left-32 -translate-x-1/2">
-        <div className="relative w-60.75  ml-3 h-60   overflow-hidden border-2 border-green-500">
+        <div className="relative w-60.75 g ml-3 h-60   overflow-hidden border-2 border-green-500">
           <Image src={image} alt={name} fill className="object-cover" />
         </div>
       </div>
